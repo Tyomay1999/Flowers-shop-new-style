c6c40433929c6e3637c2507774fb4adb7ef0fb56
Tyomay1999 <artyom.bordulanyuk@beeoncode.com>
2022-05-29 12:25:12 +0300

    change greeting background image

--- Components/Greetings/greetings.js
+++ Components/Greetings/greetings.js
@@ -2,11 +2,14 @@ import React from 'react'
 import greetingsStyles from './greetings.module.scss'
 import { useRouter } from "next/router";
 import {greetings_section} from "../Common/web-site-static-words";
+import greetingBackground from '../../public/Assets/images/greetings-bg.jpg'
+
 
 const Greetings = () => {
     const router = useRouter()
     return (
         <section className={ greetingsStyles.greetings } id="greetings">
+            <img alt='greeting image' src={greetingBackground} />
             <div className={ greetingsStyles.content }>
                 <h3>{greetings_section.heading}</h3>
                 <span> {greetings_section.title} </span>
